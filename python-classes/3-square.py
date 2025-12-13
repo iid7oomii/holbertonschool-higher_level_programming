c73a5b08fd07a223c06dacd58dea58ff35186bc2
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-12-13 13:52:39 +0000

    Task-3 fix pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -28,12 +28,11 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
         # Private instance attribute storage
         self.__size = size
 
