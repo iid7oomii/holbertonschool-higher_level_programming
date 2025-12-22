8ee0f280be44f59d7295f50e97479319e1385748
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-12-22 15:05:00 +0000

    return json representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyton3
+"""import json to deal wit json representation"""
+import json
+
+
+def to_json_string(my_obj):
+    """return json repersentation of an object"""
+    return (json.dumps(my_obj))
