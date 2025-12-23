2676df404009ae8f199ad358bc2f5d70da9fbfb7
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-12-23 20:04:02 +0000

    write function return dictionary description

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""write function returns dictionary description"""
+
+
+def class_to_json(obj):
+    """return dictionary description with sample data structure"""
+    return obj.__dict__
