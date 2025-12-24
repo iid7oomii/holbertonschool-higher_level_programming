60e925583d1461d3a4d7b3c7de522ae6efb42edc
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-12-24 10:33:55 +0000

    write class defines student by first name, last name and age

---
+++ python-input_output/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. تثبيت البرامج الضرورية
+echo "Installing pycodestyle..."
+pip install pycodestyle
+
+echo "Installing tmux..."
+sudo apt install tmux -y
+
+# 2. إعدادات Git (بياناتك الشخصية)
+echo "Configuring Git..."
+git config --global user.email "abdulrahman18alghamdi@gmail.com"
+git config --global user.name "iid7oomii"
+git config --global credential.helper store
+
+echo "✅ All Done! Environment is ready."
