d42dd2419991e1ee93c1856d85bc62c1a3cfc082
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-12-24 11:43:54 +0000

    defines the user input and check if it is list of attrs

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if(isinstance(attrs, list) and
+                all(isinstance(x, str) for x in attrs)):
+
+            new_dict ={}
+
+            for key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+
+                return new_dict
+
+            else:
+                return self.__dict__
