c275fe0854428c43686efe7c3180541384b9ee62
iid7oomii <abdulrahman18alghamdi@gmail.com>
2025-12-24 19:15:03 +0000

    crfix documentation error

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
     """
-    return a list of integers representing pascal`s triangle
+    Defines a function that returns a list of integers
+    representing the Pascal`s triangle of n
     """
 
     if n <= 0:
