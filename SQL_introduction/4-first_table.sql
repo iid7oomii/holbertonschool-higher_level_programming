ba68e72ebfc822a5af55583d71d9b3660551b955
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-15 20:42:49 +0000

    Task - 4 creates a table called first_table with the description

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- script that creates a table called first_table with id INT name VARCHAR(256) description
+-- use create table if not exists (id type, name type);
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+CREATE TABLE IF NOT EXISTS first_table (
+id INT ,
+name VARCHAR(256)
+);
