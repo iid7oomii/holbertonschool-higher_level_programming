75dcdf4ef08806d7b810badacee2dc8be02c62a5
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-15 20:47:04 +0000

    Task - 4 Add PRIMARY KEY

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 -- script that creates a table called first_table with id INT name VARCHAR(256) description
 -- use create table if not exists (id type, name type);
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
-id INT ,
+id INT PRIMARY KEY ,
 name VARCHAR(256)
 );
