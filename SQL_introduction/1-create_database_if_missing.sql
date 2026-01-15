a1558ad50bf6b15a30818b95e3fcf4491d772a6c
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-15 19:12:30 +0000

    Task-1 creates the database hbtn_0c_0 even if the database already exists

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0c_0 in your MySQL server even if hbtn_0c_0 already exists, your script should not fail
+-- use create database if not exsits
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
