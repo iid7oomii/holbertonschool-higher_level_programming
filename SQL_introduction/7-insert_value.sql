f6b62fca8cc25c1db32462a6ecc014facf9dd6a7
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-16 01:16:45 +0000

    use Alter table

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,5 @@
 -- inserts a new row in the table first_table
--- use insert into
-INSERT INTO first_table (id, name)
-VALUES (89, Best School);
+-- use alter table statement
+ALTER TABLE first_table
+ADD id = 89;
+ADD name = Best School;
