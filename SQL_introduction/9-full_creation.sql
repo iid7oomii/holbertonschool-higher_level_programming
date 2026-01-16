340e171fbd642d7ccdd866cd5d687166170fc156
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-16 01:53:54 +0000

    Task - 9 Write a script that creates a table second_table with description and records

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- Write a script that creates a table second_table with description and records
+-- use , to add more values at the same time
+CREATE TABLE IF NOT EXISTS second_table (
+id INT PRIMARY KEY ,
+name VARCHAR(256) ,
+score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10) , (2, "Alex", 3) , (3, "Bob", 14) , (4, "George", 8;);
