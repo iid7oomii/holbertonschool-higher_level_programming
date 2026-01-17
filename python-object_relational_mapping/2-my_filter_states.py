c8d6f8cef86cae21750380816df85ea7d5adfab5
Abdulrahman Alghamdi <A.D.Alghamdi@outlook.com>
2026-01-17 07:28:46 +0300

    Improve comments for state filtering script
    Updated the comments to clarify usage and functionality.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Displays states where the name matches the given argument."""
-
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
 import sys
 import MySQLdb
 
