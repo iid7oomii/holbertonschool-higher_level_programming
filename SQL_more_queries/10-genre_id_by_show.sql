a8f3f66403817f6cbb58061a3acf4dd446166a8e
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-17 03:46:39 +0000

    Task-10 fix problem

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,8 @@
--- 10-genre_id_by_show.sql
--- List all TV shows that have at least one genre linked
--- Each record shows: tv_shows.title - tv_show_genres.genre_id
--- Results are sorted by tv_shows.title and tv_show_genres.genre_id in ascending order
--- Only one SELECT statement is used
-
+-- lists all shows that have at least one genre linked
+-- displays: tv_shows.title - tv_show_genres.genre_id
+-- sorted by tv_shows.title ASC and tv_show_genres.genre_id ASC
+-- uses only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
