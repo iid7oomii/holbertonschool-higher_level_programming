20746ec285993cff371af80733bf007754fbd634
Abdulrahman Alghamdi <A.D.Alghamdi@outlook.com>
2026-01-17 07:30:28 +0300

    Refine docstring for state filtering script
    Updated the docstring to clarify usage and purpose.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""Displays states where the name matches the given argument."""
+
 import sys
 import MySQLdb
 
