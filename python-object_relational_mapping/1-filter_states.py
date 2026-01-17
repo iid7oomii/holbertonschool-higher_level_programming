01f841bdb8b2675e8c1966eec3606063dca1b6fb
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-17 04:09:32 +0000

    fix problem

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-
-# This script lists all states with a name starting with 'N'
-# from the database hbtn_0e_0_usa. Results are sorted by states.id in ascending order.
-
-import MySQLdb
+"""
+This script lists all states with a name starting with 'N'
+from the database hbtn_0e_0_usa. Results are sorted by states.id in ascending order.
+It takes 3 arguments: mysql username, mysql password, and database name.
+"""
 import sys
+import MySQLdb
 
-# Get MySQL username, password and database name from command line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
-
-# Connect to MySQL server on localhost at port 3306
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=username, passwd=password, db=database)
-cursor = db.cursor()
-
-# Execute SQL query to select states where name starts with 'N' and order by id
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-# Fetch all the resulting rows
-rows = cursor.fetchall()
-
-# Print each row
-for row in rows:
-    print(row)
-
-# Close the cursor and the database connection
-cursor.close()
-db.close()
-
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
