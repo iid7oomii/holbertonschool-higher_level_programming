a65f7499b8b45d73c5eefd9299ca62e2842c5ce5
iid7oomii <abdulrahman18alghamdi@gmail.com>
2026-01-17 02:18:26 +0000

    add if not exists

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- creates the table force_name with description and name not null
 -- use create table and not null
-CREATE TABLE force_name (
+CREATE TABLE IF NOT EXISTS force_name (
 id INT PRIMARY KEY ,
 name VARCHAR(256) NOT NULL
 );
